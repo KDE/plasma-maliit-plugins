KDE/plasma-maliit-plugins

commit 54e57c79182b47a116d2a0e83ed48fdeb0748ba4

Changes: Disable KBA's while animating a language switch
RevBy: TrustMe
Details: Keys that move underneath an otherwise static touchpoint would
require too much special logic to be handled correctly. It is much easier
to simply disable KBA's during animations.

m-keyboard/widgets/horizontalswitcher.cpp
@@ -77,9 +77,11 @@ void HorizontalSwitcher::switchTo(SwitchDirection direction)
 
     // Current item is about to leave
     leaveAnim.setItem(currentWidget);
+    currentWidget->setEnabled(false);
 
     // New item is about to enter
     enterAnim.setItem(nextWidget);
+    nextWidget->setEnabled(false);
 
     // Try to fit current size.
     nextWidget->resize(size());
@@ -278,6 +280,10 @@ void HorizontalSwitcher::finishAnimation()
 
     // Discard cached sizehint info before telling that the switch is done.
     updateGeometry();
+
+    if (currentWidget()) {
+        currentWidget()->setEnabled(true);
+    }
 
     emit switchDone(oldIndex, currentIndex);
     emit switchDone(old, slides.at(currentIndex));

commit f48a79e1689285768031cc68c2a37ba96c3dc044

Fixes: Disabled Symview pages
RevBy: Ning Chi
Details: This sequence left the user with an unusable Symview (regression
caused by commit 54e57c79):
1. Open VKB + Symview,
2. Navigate to second Symview page,
3. Close VKB (eg, "Done"),
4. Open VKB + Symview

The reason was that the HorizontalSwitcher forgot to enable the old widget
again, after the animation finished.

m-keyboard/widgets/horizontalswitcher.cpp
@@ -260,7 +260,6 @@ QSizeF HorizontalSwitcher::sizeHint(Qt::SizeHint which, const QSizeF &constraint
     return hint;
 }
 
-
 void HorizontalSwitcher::finishAnimation()
 {
     int oldIndex = -1;
@@ -269,6 +268,7 @@ void HorizontalSwitcher::finishAnimation()
     QGraphicsWidget *old = static_cast<QGraphicsWidget *>(leaveAnim.item());
     if (old) {
         oldIndex = slides.indexOf(old);
+        old->setEnabled(true);
         old->hide();
     }
 

tests/ut_horizontalswitcher/ut_horizontalswitcher.cpp
@@ -18,6 +18,8 @@
 #include "horizontalswitcher.h"
 #include "utils.h"
 
+#include <QGraphicsView>
+#include <QGraphicsScene>
 #include <QApplication>
 #include <QPointer>
 
@@ -26,8 +28,9 @@ Q_DECLARE_METATYPE(QGraphicsWidget *);
 
 void Ut_HorizontalSwitcher::initTestCase()
 {
-    static int argc = 1;
-    static char *app_name[1] = { (char *) "ut_horizontalswitcher" };
+    static int argc = 2;
+    static char *app_name[2] = { (char *) "ut_horizontalswitcher",
+                                 (char *) "-software"};
 
     disableQtPlugins();
     app = new QApplication(argc, app_name);
@@ -186,6 +189,49 @@ void Ut_HorizontalSwitcher::testSwitchLeftRight()
     subject->switchTo(direction);
     QCOMPARE(subject->current(), expectedIndex);
     QCOMPARE(subject->currentWidget(), expectedWidget);
+}
+
+void Ut_HorizontalSwitcher::testDisabledDuringTransitions_data()
+{
+    QTest::addColumn<bool>("playAnimation");
+    QTest::newRow("without animation") << false;
+    QTest::newRow("with animation") << true;
+}
+
+void Ut_HorizontalSwitcher::testDisabledDuringTransitions()
+{
+    QFETCH(bool, playAnimation);
+
+    QGraphicsView view;
+    view.setScene(new QGraphicsScene);
+    view.show();
+
+    QGraphicsWidget *root  = new QGraphicsWidget;
+    QGraphicsWidget *left  = new QGraphicsWidget(root);
+    QGraphicsWidget *right = new QGraphicsWidget(root);
+    QSignalSpy spy(subject, SIGNAL(switchDone(QGraphicsWidget*, QGraphicsWidget*)));
+
+    view.scene()->addItem(root);
+    subject->setDuration(1);
+    subject->setAnimationEnabled(playAnimation);
+    subject->addWidget(left);
+    subject->addWidget(right);
+
+    QCOMPARE(subject->currentWidget(), left);
+    QVERIFY(left->isEnabled());
+    QVERIFY(right->isEnabled());
+
+    subject->switchTo(HorizontalSwitcher::Right);
+    waitForSignal(subject, SIGNAL(switchDone(QGraphicsWidget*, QGraphicsWidget*)), 1000);
+
+    QCOMPARE(subject->currentWidget(), right);
+    QVERIFY(left->isEnabled());
+    QVERIFY(right->isEnabled());
+
+    root->hide();
+    root->show();
+    QVERIFY(left->isEnabled());
+    QVERIFY(right->isEnabled());
 }
 
 // comment below test cases due to MCompositor bug: NB#182701 breaks us

tests/ut_horizontalswitcher/ut_horizontalswitcher.h
@@ -42,6 +42,8 @@ private slots:
     void testShowWidgets();
     void testSwitchLeftRight_data();
     void testSwitchLeftRight();
+    void testDisabledDuringTransitions_data();
+    void testDisabledDuringTransitions();
 // comment below test cases due to MCompositor bug: NB#182701 breaks us
 #if 0
     void testSwitchSignals();

tests/ut_mkeyboardhost/ut_mkeyboardhost.cpp
@@ -98,15 +98,6 @@ Q_DECLARE_METATYPE(ModifierState)
 Q_DECLARE_METATYPE(Ut_MKeyboardHost::TestOpList)
 Q_DECLARE_METATYPE(MInputMethod::InputModeIndicator)
 
-static void waitForSignal(const QObject* object, const char* signal, int timeout = 500)
-{
-    QEventLoop eventLoop;
-    QObject::connect(object, signal, &eventLoop, SLOT(quit()));
-    QTimer::singleShot(timeout, &eventLoop, SLOT(quit()));
-    eventLoop.exec();
-}
-
-
 // Stubbing..................................................................
 
 void MVirtualKeyboard::setKeyboardState(MInputMethod::HandlerState state)

tests/utils/utils.cpp
@@ -22,6 +22,9 @@
 #include <mplainwindow.h>
 #include <MSceneWindow>
 #endif
+#include <QObject>
+#include <QTimer>
+#include <QEventLoop>
 #include <QApplication>
 #include <QCommonStyle>
 
@@ -41,6 +44,16 @@ void disableQtPlugins()
     // TODO: Check if hardwiring the QStyle can be removed at a later state.
     QApplication::setStyle(new QCommonStyle);
 }
+
+// Wait for signal or timeout; use SIGNAL macro for signal
+void waitForSignal(const QObject* object, const char* signal, int timeout)
+{
+    QEventLoop eventLoop;
+    QObject::connect(object, signal, &eventLoop, SLOT(quit()));
+    QTimer::singleShot(timeout, &eventLoop, SLOT(quit()));
+    eventLoop.exec();
+}
+
 
 // Create graphics scene
 #ifdef MEEGOTOUCH

tests/utils/utils.h
@@ -18,9 +18,13 @@
 #ifndef UTILS_H
 
 class MPlainWindow;
+class QObject;
 
 // Disable loading of MInputContext and QtMaemo6Style
 void disableQtPlugins();
+
+// Wait for signal or timeout; use SIGNAL macro for signal
+void waitForSignal(const QObject* object, const char* signal, int timeout = 500);
 
 #ifdef MEEGOTOUCH
 // Create graphics scene