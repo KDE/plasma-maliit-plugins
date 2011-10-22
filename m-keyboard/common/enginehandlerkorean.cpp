ed2b6abeda37af4cc68ff90214a7634d14d0fbe1
Hoseok Chang <hoseok@hoseok.kr>
2011-10-22 12:52:33 +0300

    Changes: Split out Hangul engine handler into separate files
    RevBy: Ning Chi, Mohammad Anwari, Michael Hasselmann

---
+++ m-keyboard/common/enginehandlerkorean.cpp
@@ -0,0 +1,271 @@
+
+#include "enginehandlerkorean.h"
+#include "mabstractinputmethodhost.h"
+#include "enginemanager.h"
+#include "mimcorrectionhost.h"
+#include "mkeyboardhost.h"
+#include <mimenginefactory.h>
+#include <MGConfItem>
+#include <MSceneWindow>
+
+namespace
+{
+    const bool DefaultCorrectionSettingAcceptedWithSpaceOption = false;
+    const QString CorrectionSettingWithSpace("/meegotouch/inputmethods/virtualkeyboard/correctwithspace");
+}
+
+EngineHandlerKorean::EngineHandlerKorean(MKeyboardHost &keyboardHost)
+    : EngineHandler(keyboardHost),
+      mKeyboardHost(keyboardHost),
+      mEngineWidgetHost(new MImCorrectionHost(keyboardHost.sceneWindow, 0))
+{
+    engine = EngineManager::instance().engine();
+}
+ 
+EngineHandlerKorean::~EngineHandlerKorean()
+{
+}
+    
+QStringList EngineHandlerKorean::supportedLanguages()
+{
+    QStringList languages;
+    languages << "ko";
+    return languages;
+}
+
+//! \reimp
+void EngineHandlerKorean::activate()
+{
+    connect(mEngineWidgetHost,
+        SIGNAL(candidateClicked(const QString &, int)), &mKeyboardHost,
+        SLOT(handleCandidateClicked(const QString &, int)), 
+        Qt::UniqueConnection);
+    mEngineWidgetHost->finalizeOrientationChange();
+}
+
+void EngineHandlerKorean::deactivate()
+{
+    disconnect(mEngineWidgetHost, 0, &mKeyboardHost,    0);
+}
+
+AbstractEngineWidgetHost *EngineHandlerKorean::engineWidgetHost()
+{
+    // return default error correction host
+    return mEngineWidgetHost;
+}
+
+bool EngineHandlerKorean::cursorCanMoveInsidePreedit() const
+{
+    return false;
+}
+
+bool EngineHandlerKorean::hasHwKeyboardIndicator() const
+{
+    return true;
+}
+
+bool EngineHandlerKorean::hasErrorCorrection() const
+{
+    return true;
+}
+
+bool EngineHandlerKorean::acceptPreeditInjection() const
+{
+    return true;
+}
+
+bool EngineHandlerKorean::hasAutoCaps() const
+{
+    return false;
+}
+
+QList<QRegExp> EngineHandlerKorean::autoCapsTriggers() const
+{
+    return QList<QRegExp>();
+}
+
+bool EngineHandlerKorean::hasContext() const
+{
+    return false;
+}
+
+bool EngineHandlerKorean::commitPreeditWhenInterrupted() const
+{
+    return true;
+}
+
+bool EngineHandlerKorean::correctionAcceptedWithSpaceEnabled() const
+{
+    return MGConfItem(CorrectionSettingWithSpace)
+        .value(DefaultCorrectionSettingAcceptedWithSpaceOption).toBool();
+}
+
+bool EngineHandlerKorean::isComposingInputMethod() const
+{
+    return false;
+}
+
+bool EngineHandlerKorean::supportTouchPointAccuracy() const
+{
+    return true;
+}
+
+bool EngineHandlerKorean::commitWhenCandidateClicked() const
+{
+    return true;
+}
+
+void EngineHandlerKorean::clearPreedit(bool commit)
+{
+    if (!mKeyboardHost.preedit.isEmpty()) {
+        if (commit) {
+            // Commit current preedit
+            mKeyboardHost.inputMethodHost()
+                ->sendCommitString(mKeyboardHost.preedit, 0, 0, 
+                mKeyboardHost.preeditCursorPos);
+        } else {
+            // Clear current preedit
+            QList<MInputMethod::PreeditTextFormat> preeditFormats;
+            MInputMethod::PreeditTextFormat preeditFormat(0, 0, 
+                MInputMethod::PreeditKeyPress);
+            preeditFormats << preeditFormat;
+            mKeyboardHost.inputMethodHost()->sendPreeditString("", 
+                preeditFormats);
+        }
+        mKeyboardHost.preedit.clear();
+    }
+}
+
+void EngineHandlerKorean::editingInterrupted()
+{
+    clearPreedit(commitPreeditWhenInterrupted());
+}
+
+void EngineHandlerKorean::resetHandler()
+{
+}
+
+void EngineHandlerKorean::preparePluginSwitching()
+{
+}
+
+bool EngineHandlerKorean::handleKeyPress(const KeyEvent &event)
+{
+    Q_UNUSED(event);
+    return false;
+}
+
+bool EngineHandlerKorean::handleKeyRelease(const KeyEvent &event)
+{
+    Q_UNUSED(event);
+    return false;
+}
+
+bool EngineHandlerKorean::handleKeyClick(const KeyEvent &event, bool cycleKeyActive)
+{
+    Q_UNUSED(cycleKeyActive);
+    QChar typedChar = event.text().at(0);
+    KeyEvent::SpecialKey specialkey = event.specialKey();
+    Qt::Key qtkey = event.qtKey();
+    
+    if ((specialkey == KeyEvent::LayoutMenu)
+        || (specialkey == KeyEvent::Sym) 
+        || (specialkey == KeyEvent::OnOffToggle)
+        || (qtkey == Qt::Key_Return)
+        || (specialkey == KeyEvent::Commit)
+        || (qtkey == Qt::Key_Space)) {
+        flushOut();
+        return false;
+    } else if (qtkey == Qt::Key_Backspace) {
+        // Hangul engine cares backspace when there is something in preedit.
+        if (getPreedit().length() == 0) {
+          return false;
+        } else {
+          typedChar = '\b';
+        }
+    } else if (typedChar.toAscii() != 0) {
+        flushOut();
+        return false;
+    }
+    
+    engine->appendCharacter(typedChar);
+    
+    const QString commitString = getCommit();
+    const QString preeditString = getPreedit();
+    
+    if (!commitString.isEmpty()) {
+        sendCommit(commitString);
+        engine->setSuggestedCandidateIndex(0); // always choose the first one
+    }
+    sendPreedit(preeditString);
+    
+    return true;
+}
+
+bool EngineHandlerKorean::handleKeyCancel(const KeyEvent &event)
+{
+    Q_UNUSED(event);
+    return false;
+}
+
+//! \reimp_end
+
+void EngineHandlerKorean::sendPreedit(const QString & str)
+{
+    QList < MInputMethod::PreeditTextFormat > preeditFormats;
+    MInputMethod::PreeditTextFormat preeditFormat(0, str.length(), 
+        MInputMethod::PreeditKeyPress);
+    preeditFormats << preeditFormat;
+    // preedit length is always 1
+    mKeyboardHost.inputMethodHost()->sendPreeditString(str, preeditFormats,
+        0, 0, -1);
+}
+
+void EngineHandlerKorean::sendCommit(const QString & str)
+{
+    if (str.length() > 0)
+        mKeyboardHost.inputMethodHost()->sendCommitString(str, 0, 0, -1);
+    // send commited signal.
+    // Hangul engine will clear commit string but keep preedit string
+    engine->setSuggestedCandidateIndex(0);
+}
+
+QString EngineHandlerKorean::getPreedit()
+{
+    QStringList list;
+    list = engine->candidates(0, 0);
+    if (!list.isEmpty()) {
+        QString str = list.at(0);
+        // preedit character is the last 1 character of the only candidate
+        return str.right(1);
+    } else {
+        return QString();
+    }
+}
+    
+QString EngineHandlerKorean::getCommit()
+{
+    QStringList list;
+    list = engine->candidates(0, 0);
+    if (!list.isEmpty()) {
+        QString str = list.at(0);
+        // commit character is the first characters if any
+        return str.length() <= 1 ? QString() : str.left(str.length() - 1);
+    } else {
+        return QString();
+    }
+}
+
+void EngineHandlerKorean::flushOut() 
+{
+    QString str = getCommit();
+    str.append(getPreedit());
+    sendCommit(str);
+    clearCandidates();
+}
+
+void EngineHandlerKorean::clearCandidates()
+{
+    engine->clearEngineBuffer();
+}
+
---
+++ m-keyboard/common/enginehandlerkorean.h
@@ -0,0 +1,80 @@
+#include "mabstractinputmethodhost.h"
+#include "enginemanager.h"
+#include "mimcorrectionhost.h"
+#include "mkeyboardhost.h"
+#include <mimenginefactory.h>
+#include <MGConfItem>
+#include <MSceneWindow>
+
+/*
+ * Korean (Hangul) input engine handler class
+ *
+ * One Korean character is a composition of multiple Korean alphabets.
+ * Each single Korean character has its unique sequence of typings.
+ * Korean input system needs to compose the sequence into a character
+ * and display the procedure of the composition.
+ * ex. "ㅎ", "ㅏ" and "ㄴ" becomes "한". Users see "ㅎ", "하" and "한" 
+ * in the preedit area as they type.
+ *
+ * Hangul input engine produces commit and preedit characters 
+ * in every key input. The engine handler should take those in every input 
+ * and show them properly.
+ *
+ * The handler assume that Hangul engine produce a single candidate with
+ * a commit character and a preedit character. The commit charater is 
+ * available when a Korean character is composed completely.
+ * So if the length of candidate string is 1, this means the candidate string 
+ * is preedit string. If the length of candidate is 2, the first character is 
+ * commit and the last one is preedit.
+ *
+ * EngineHandlerKorean is based on the most famous Hangul engine: libhangul.
+ *
+ */
+
+class EngineHandlerKorean : public EngineHandler
+{
+public:
+    EngineHandlerKorean(MKeyboardHost &keyboardHost);
+    virtual ~EngineHandlerKorean();
+    static QStringList supportedLanguages();
+
+    //! \reimp
+    virtual void activate();
+    virtual void deactivate();
+    virtual AbstractEngineWidgetHost *engineWidgetHost();
+    virtual bool cursorCanMoveInsidePreedit() const;
+    virtual bool hasHwKeyboardIndicator() const;
+    virtual bool hasErrorCorrection() const;
+    virtual bool acceptPreeditInjection() const;
+    virtual bool hasAutoCaps() const;
+    virtual QList<QRegExp> autoCapsTriggers() const;
+    virtual bool hasContext() const;
+    virtual bool commitPreeditWhenInterrupted() const;
+    virtual bool correctionAcceptedWithSpaceEnabled() const;
+    virtual bool isComposingInputMethod() const;
+    virtual bool supportTouchPointAccuracy() const;
+    virtual bool commitWhenCandidateClicked() const;
+    virtual void clearPreedit(bool commit);
+    virtual void editingInterrupted();
+    virtual void resetHandler();
+    virtual void preparePluginSwitching();
+    virtual bool handleKeyPress(const KeyEvent &event);
+    virtual bool handleKeyRelease(const KeyEvent &event);
+    virtual bool handleKeyClick(const KeyEvent &event, bool cycleKeyActive);
+    virtual bool handleKeyCancel(const KeyEvent &event);
+    //! \reimp_end
+
+private:
+    void sendPreedit(const QString & str);
+    void sendCommit(const QString & str);
+    QString getPreedit();
+    QString getCommit();
+    void flushOut();
+    void clearCandidates();
+
+private:
+    MKeyboardHost &mKeyboardHost;
+    AbstractEngineWidgetHost *mEngineWidgetHost;
+    MImEngineWordsInterface *engine;
+};
+
