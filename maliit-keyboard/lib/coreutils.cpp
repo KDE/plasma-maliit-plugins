66a4040bde986e4d33e326ccbf3c428b9581b2d4
Michael Hasselmann <michaelh@openismus.com>
2012-09-06 19:40:59 +0200

    Coding style: Fix variable names in coreutils.cpp
    RevBy: Krzesimir Nowak
    
    This work was done by Openismus GmbH for Canonical Ltd

--- maliit-keyboard/lib/coreutils.cpp
+++ maliit-keyboard/lib/coreutils.cpp
@@ -12,33 +12,33 @@ const char *const g_action_key_id = "actionKey";
 } // unnamed namespace
 
 const QString &pluginDataDirectory() {
-    static QString pluginDataDirectory;
+    static QString data_directory;
 
-    if (pluginDataDirectory.isNull()) {
-        QByteArray envVar = qgetenv("MALIIT_PLUGINS_DATADIR");
-        if (envVar.isEmpty()) {
-            pluginDataDirectory = QString::fromUtf8(MALIIT_PLUGINS_DATA_DIR);
+    if (data_directory.isNull()) {
+        QByteArray env_data_directory = qgetenv("MALIIT_PLUGINS_DATADIR");
+        if (env_data_directory.isEmpty()) {
+            data_directory = QString::fromUtf8(MALIIT_PLUGINS_DATA_DIR);
         } else {
-            pluginDataDirectory = QString::fromUtf8(envVar);
+            data_directory = QString::fromUtf8(env_data_directory);
         }
     }
 
-    return pluginDataDirectory;
+    return data_directory;
 }
 
 const QString &maliitKeyboardDataDirectory() {
-    static QString maliitKeyboardDataDirectory;
+    static QString data_directory;
 
-    if (maliitKeyboardDataDirectory.isNull()) {
-        QByteArray envVar = qgetenv("MALIIT_KEYBOARD_DATADIR");
-        if (envVar.isEmpty()) {
-            maliitKeyboardDataDirectory = QString::fromUtf8(MALIIT_KEYBOARD_DATA_DIR);
+    if (data_directory.isNull()) {
+        QByteArray env_data_directory = qgetenv("MALIIT_KEYBOARD_DATADIR");
+        if (env_data_directory.isEmpty()) {
+            data_directory = QString::fromUtf8(MALIIT_KEYBOARD_DATA_DIR);
         } else {
-            maliitKeyboardDataDirectory = QString::fromUtf8(envVar);
+            data_directory = QString::fromUtf8(env_data_directory);
         }
     }
 
-    return maliitKeyboardDataDirectory;
+    return data_directory;
 }
 
 QString idFromKey(const Key &key)
