0dc6598444a6f957e2a1c07b7056c0335d7cbcbe
Marco Martin <notmart@gmail.com>
2014-12-10 18:34:54 +0100

    Revert "build"
    This reverts commit f741a4a10911ada994a0a94ee69cf6fd2bba3577.

--- maliit-keyboard/lib/models/styleattributes.cpp
+++ maliit-keyboard/lib/models/styleattributes.cpp
@@ -31,7 +31,6 @@
  */
 
 #include "styleattributes.h"
-#include <QGuiApplication>
 
 //! \class StyleAttributes
 //! This class allows to query style attributes, such as image names and font
@@ -204,7 +203,7 @@ StyleAttributes::StyleAttributes(const QSettings *store)
             m_style_name.toLocal8Bit(),
             QByteArray("key-area-width")).toReal();
 
-    m_scale_factor = (qreal)QGuiApplication::primaryScreen()->size().width() / width;
+    m_scale_factor = (qreal)qApp->primaryScreen()->size().width() / width;
 }
 
 //! \brief Destructor
